361f13e9072b814847736c078005f1cf5776910c
MartinZikla <zikla@stud.uniza.sj>
2024-06-03 18:41:46 +0200

    Redundant functions removed

--- app/src/main/java/com/example/sp_fitapp01/MainActivity.kt
+++ app/src/main/java/com/example/sp_fitapp01/MainActivity.kt
@@ -3,10 +3,6 @@ package com.example.sp_fitapp01
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.sp_fitapp01.ui.theme.SPFitApp01Theme
 
 class MainActivity : ComponentActivity() {
